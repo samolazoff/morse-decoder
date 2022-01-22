8778bd03ec2cb1a7827a05d4b5bec8e380b83f6e
Samalazau Raman <samolazov.roman.alex@gmail.com>
2022-01-22 11:19:47 +0300

    feat:add the solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,27 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let rez='';
+    for(let i=0;i<expr.length;i+=10){
+      let word=expr.slice(i,i+10);
+      let arr=[];
+      for(let i=0;i<10;i+=2){
+        if(word=='**********'){
+          arr.push(' ');
+          break;
+        }
+        if(word[i]!=0){
+          arr.push(word.slice(i,i+2)=='11'?'-':'.');
+        }
+      }
+      rez=rez+MORSE_TABLE[arr.join('')]
+    }
+    
+    return rez;
 }
 
 module.exports = {
